84e76564061d053342b0db80115c87d23de748ad
Elena Ardura <202111181@alu.comillas.edu>
2025-05-05 08:32:40 +0200

    Attempt 1

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../../icai2025-5f9879654f17.json")
+  credentials = file("../../final-exam-elenaardura-069b5315e4f4.json")
 
   project = var.gcp-project
   region  = var.gcp-region
